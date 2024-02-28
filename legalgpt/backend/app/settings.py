0bf39f7e4c3489c6886de4ed7edc1b65de51967a
YeonwooSung <neos960518@gmail.com>
2024-02-28 14:44:39 +0900

    feat: Use gpt-4 as default

--- legalgpt/backend/app/settings.py
+++ legalgpt/backend/app/settings.py
@@ -1,10 +1,11 @@
 import os
-from llama_index.llms.openai import OpenAI
+
 from llama_index.core.settings import Settings
+from llama_index.llms.openai import OpenAI
 
 
 def init_settings():
-    model = os.getenv("MODEL", "gpt-3.5-turbo")
+    model = os.getenv("MODEL", "gpt-4")
     Settings.llm = OpenAI(model=model)
     Settings.chunk_size = 1024
     Settings.chunk_overlap = 20
