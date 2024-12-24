12850aaf2cd159f768bff8f8c9b02cf7a0270f81
YeonwooSung <neos960518@gmail.com>
2024-12-24 13:16:15 +0900

    fix: Fix up typo

--- server/app/api/chat/router.py
+++ server/app/api/chat/router.py
@@ -5,7 +5,7 @@ from llama_index.core.llms import ChatMessage, MessageRole
 from app.engine import get_chat_engine
 
 # custom modules
-from .schema import ChatData, ChatMessage
+from .schema import ChatData
 
 
 chat_router = r = APIRouter()
