009586099572faf176511af613f5474192509e6d
YeonwooSung <neos960518@gmail.com>
2024-12-26 14:59:57 +0900

    add support for hyde, not using it yet, but will use it in near future!

--- server/app/engine/__init__.py
+++ server/app/engine/__init__.py
@@ -1,8 +1,17 @@
 from llama_index.core import PromptTemplate
 from llama_index.core.chat_engine import CondenseQuestionChatEngine
+from llama_index.core.query_engine import TransformQueryEngine
+from llama_index.core.indices.query.query_transform import HyDEQueryTransform
 
 # custom modules
 from app.engine.index import get_index
+
+
+def _use_hyde(query_engine, use_hyde: bool):
+    if use_hyde:
+        hyde = HyDEQueryTransform(include_original=True)
+        query_engine = TransformQueryEngine(query_engine, hyde)
+    return query_engine
 
 
 def get_chat_engine():
@@ -13,9 +22,10 @@ def get_chat_engine():
     )
 
 
-def get_custom_chat_engine(last_msg: str, chat_history: list, verbose: bool = False):
+def get_custom_chat_engine(last_msg: str, chat_history: list, verbose: bool = False, use_hyde: bool = False):
     index = get_index()
     query_engine = index.as_query_engine()
+    query_engine = _use_hyde(query_engine, use_hyde=use_hyde)
 
     custom_prompt_str = f"""As a user, I want to know more about "{last_msg}".
     Answer in same language as the question. If you think the question is ambiguous, please ask for clarification.
